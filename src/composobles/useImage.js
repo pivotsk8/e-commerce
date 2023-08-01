54dd154f2924368d8a0ee56fa1d3fa3a4fa56488
piiivot <the_pivotsk8@hotmail.com>
2023-08-01 11:02:37 -0500

    feat: implementation function for upload image in firebase

--- src/composobles/useImage.js
+++ src/composobles/useImage.js
@@ -1,9 +1,45 @@
+import { ref, computed } from 'vue';
+import { uid } from 'uid';
+import { useFirebaseStorage } from 'vuefire';
+import {
+  ref as storageRef,
+  uploadBytesResumable,
+  getDownloadURL,
+} from 'firebase/storage';
+
 export default function useImage() {
+  const url = ref('');
+  const storage = useFirebaseStorage();
+
   const onFileChange = (e) => {
-    console.log(e.target.files[0]);
+    const {
+      target: { files },
+    } = e;
+    const filename = uid() + '.jpg';
+    const sRef = storageRef(storage, '/priducts/' + filename);
+
+    //Sube el archivo
+    const uploadTask = uploadBytesResumable(sRef, files[0]);
+    uploadTask.on(
+      'state_changed',
+      () => {},
+      (error) => console.log(error),
+      () => {
+        //Upload is complete
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          url.value = downloadURL;
+        });
+      },
+    );
   };
 
+  const isImageUploaded = computed(() => {
+    return url.value ? url.value : null;
+  });
+
   return {
+    url,
+    isImageUploaded,
     onFileChange,
   };
 }
--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -2,7 +2,7 @@
   import useImage from '../../composobles/useImage';
   import Link from '@/components/Link.vue';
 
-  const { onFileChange } = useImage();
+  const { onFileChange, url, isImageUploaded } = useImage();
 </script>
 
 <template>
@@ -13,14 +13,17 @@
 
     <div class="flex justify-center bg-white shadow">
       <div class="mt-10 p-10 w-full 2xl:w-2/4">
-        <FormKit type="form">
+        <FormKit
+          type="form"
+          submit-label="Agregar Producto"
+          incomplete-message="Verifica que todos los campos esten llenos">
           <FormKit
             type="text"
             label="Nombre"
             name="name"
             placeholder="Nombre de Producto"
             validation="required"
-            :validation-message="{
+            :validation-messages="{
               required: 'El Nombre del Producto es Obligatorio',
             }" />
 
@@ -29,19 +32,24 @@
             label="Imagen Producto"
             name="image"
             validation="required"
-            :validation-message="{
+            :validation-messages="{
               required: 'La Imagen del Producto es Obligatorio',
             }"
             accept=".jpg"
             multiple="true"
             @change="onFileChange" />
 
+          <div v-if="isImageUploaded">
+            <p class="font-black">Imagen Producto:</p>
+            <img :src="url" alt="image not found" class="w-32" />
+          </div>
+
           <FormKit
             type="select"
             label="Categoria"
             name="category"
             validation="required"
-            :validation-message="{
+            :validation-messages="{
               required: 'La Categoria es Obligatorio',
             }"
             :options="[1, 2, 3]" />
@@ -50,10 +58,9 @@
             type="number"
             label="Precio"
             name="price"
-            placeholder="Precio
-          de Producto"
+            placeholder="Precio de Producto"
             validation="required"
-            :validation-message="{
+            :validation-messages="{
               required: 'El Precio del Producto es Obligatorio',
             }"
             min="1" />
@@ -64,7 +71,7 @@
             name="availability"
             placeholder="Cantidad Diponible"
             validation="required"
-            :validation-message="{
+            :validation-messages="{
               required: 'La Cantidad es Obligatoria',
             }"
             min="1" />
