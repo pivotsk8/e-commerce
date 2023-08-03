f3ee2f7fcb6495e5717c9dd4774bb890c4534432
piiivot <the_pivotsk8@hotmail.com>
2023-08-03 11:29:32 -0500

    feat:Created in store tax and subtotal

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -25,11 +25,11 @@
       class="space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-gray-500">
       <Amount>
         <template #label>Subtotal:</template>
-        {{ formatCurrency(300) }}
+        {{ formatCurrency(cart.subTotal) }}
       </Amount>
       <Amount>
         <template #label>Impuestos:</template>
-        {{ formatCurrency(0) }}
+        {{ formatCurrency(cart.taxe) }}
       </Amount>
       <Amount>
         <template #label>Total a pagar:</template>
--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,16 +1,35 @@
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 import { defineStore } from 'pinia';
 
 export const useCartStore = defineStore('cartStore', () => {
   const items = ref([]);
+  const subTotal = ref(0);
+  const taxe = ref(0);
+
   const MAX_PRODUCTS = 5;
+  const TAX_RATE = 0.19;
+
+  watch(
+    items,
+    () => {
+      subTotal.value = items.value.reduce(
+        (total, item) => total + item.quantity * item.price,
+        0,
+      );
+
+      taxe.value = subTotal.value * TAX_RATE;
+    },
+    { deep: true },
+  );
 
   function addItem(item) {
     items.value.push({ ...item, quantity: 1, id: item.id });
   }
 
   function updateQuantity(id, quantity) {
-items.value= items.value.map(item=>item.id === id ? {...item,quantity}:item)
+    items.value = items.value.map((item) =>
+      item.id === id ? { ...item, quantity } : item,
+    );
   }
 
   const isEmpty = computed(() => items.value.length === 0);
@@ -21,6 +40,8 @@ items.value= items.value.map(item=>item.id === id ? {...item,quantity}:item)
   });
   return {
     items,
+    subTotal,
+    taxe,
     addItem,
     updateQuantity,
     isEmpty,
