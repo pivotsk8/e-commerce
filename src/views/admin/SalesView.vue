28b4492521b9dee82f9bab4bb59dac2a85e5d681
piiivot <the_pivotsk8@hotmail.com>
2023-08-08 12:12:20 -0500

    feat: configuration de datepicker tailwind

--- src/views/admin/SalesView.vue
+++ src/views/admin/SalesView.vue
@@ -1,12 +1,16 @@
 <script setup>
-
+  import VueTailWindDatePicker from 'vue-tailwind-datepicker';
 </script>
 
 <template>
-    <div>
-        <h1 class="text-4xl font-black my-10">Resumen de Ventas</h1>
+  <h1 class="text-4xl font-black my-10">Resumen de Ventas</h1>
+
+  <div class="md:flex md:items-start gap-5">
+    <div class="md:w-1/2 lg:w-1/3 bg-white flex justify-center">
+      <VueTailWindDatePicker />
     </div>
+    <div class="md:w-1/2 lg:w-2/3 pace-y-5 lg:overflow-y-scroll p-5 pb-32">
+      <p>Ventas aqui</p>
+    </div>
+  </div>
 </template>
-
-
-
